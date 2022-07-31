a837baf51038c350e922e7da1c8c07973f0ecd65
bnwyoucef <y.bounoua@esi-sba.dz>
2022-07-31 23:48:27 +0100

    calculation of ratio for progress bar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import Button from '@mui/material/Button';
 import '../styles/footer.css'
+import { useSelector } from 'react-redux';
 
 
 const Footer = () => {
+  const todoList = useSelector((state) => state.todo.todoList);
+  function todoRatio(list) {
+    let finishedTasks = 0;
+    for(let task in list) {
+      console.log(task);
+      if(list[task].completed) finishedTasks++;
+    }
+    return (finishedTasks / list.length) *100;
+  }
+  const ratio = todoRatio(todoList);
   return (
     <div className="footer-container">
-        <ProgressBar now={60} label={'60'} variant="danger" style={{flex:'3',margin:'10px',backgroundColor:'#FFC4C4'}}/>
+        <ProgressBar now={ratio} label={ratio+'%'} variant="danger" style={{flex:'3',margin:'10px',backgroundColor:'#FFC4C4'}}/>
         <Button variant="contained" style={{marginRight:'10px',backgroundColor:'#FF869E',color:'#A10035'}}>Clear</Button>
     </div>
   )
